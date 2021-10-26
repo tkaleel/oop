92adc2a052e637f3d508835a168a9569b0405aa8
Theo Kaleel <theo.kaleel@gmail.com>
2021-10-25 17:16:53 -0700

    initial commit

---
+++ fundamentals/fundamentals/strings.py
@@ -0,0 +1,7 @@
+print("this is a sample string")
+
+name = "Zen"
+print("My name is", 42)
+
+name = "Zen"
+print("My name is " +  (str(42)))
---
+++ fundamentals/introduction/hello_world.py
@@ -0,0 +1,6 @@
+print("Hellow World!")
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+
