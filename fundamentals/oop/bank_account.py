c52d8ce33c8524a0abaa0e3f8cb03af5d439b823
Theo Kaleel <theo.kaleel@gmail.com>
2021-10-26 18:43:40 -0700

    initial commit

---
+++ fundamentals/oop/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, interest_rate, account_balance=0):
+        self.interest_rate = interest_rate
+        self.account_balance = account_balance
+            
+    # adding the deposit method
+    def make_deposit(self, amount):	
+        self.account_balance += amount
+        return self	
+
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        return self
+    
+    def display_user_balance(self):
+        print("Balance: $", self.account_balance)
+
+    def yield_interest(self):
+        self.account_balance = self.account_balance+(self.account_balance*self.interest_rate)
+        return self
+
+steve = BankAccount(.01)
+sachiko = BankAccount(.01, 500)
+
+steve.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(100).yield_interest().display_user_balance()
+
+sachiko.make_deposit(1000).make_deposit(1000).make_withdrawal(1).make_withdrawal(1).make_withdrawal(1).make_withdrawal(1).yield_interest().display_user_balance()
