1096f07a5d371b64c6de983731b699f739b75660
Theo Kaleel <theo.kaleel@gmail.com>
2021-10-27 18:41:25 -0700

    add class method

--- fundamentals/oop/bank_account.py
+++ fundamentals/oop/bank_account.py
@@ -1,9 +1,10 @@
 class BankAccount:
+    accounts = []
     def __init__(self, interest_rate, account_balance=0):
         self.interest_rate = interest_rate
         self.account_balance = account_balance
+        BankAccount.accounts.append(self)
             
-    # adding the deposit method
     def make_deposit(self, amount):	
         self.account_balance += amount
         return self	
@@ -18,6 +19,11 @@ class BankAccount:
     def yield_interest(self):
         self.account_balance = self.account_balance+(self.account_balance*self.interest_rate)
         return self
+    
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_user_balance()
 
 steve = BankAccount(.01)
 sachiko = BankAccount(.01, 500)
@@ -25,3 +31,5 @@ sachiko = BankAccount(.01, 500)
 steve.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(100).yield_interest().display_user_balance()
 
 sachiko.make_deposit(1000).make_deposit(1000).make_withdrawal(1).make_withdrawal(1).make_withdrawal(1).make_withdrawal(1).yield_interest().display_user_balance()
+
+BankAccount.print_all_accounts()
